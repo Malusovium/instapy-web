2fabb85d47bf8e3f8a5a1de6a7a001e1f75dc4ae
Michiel Kamphuis <mfk@kamphuis.eu>
2018-12-28 03:56:30 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+echo "Installing Node..."
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh
+| bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo "Use Node version 10"
+nvm install 10
+nvm use 10
+
+echo "Installing docker..."
+apt install docker
+
+echo "Installing docker-compose"
+apt install docker-compose
