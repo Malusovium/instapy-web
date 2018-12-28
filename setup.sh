ad31c2aeb93e724ca2b89fc9e0cf86653baa5f86
Michiel Kamphuis <mfk@kamphuis.eu>
2018-12-28 19:16:07 +0100

    fix dependancies not installed

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ mkdir -p ./data
 echo "Cloning InstaPy project..."
 git clone https://github.com/timgrossmann/InstaPy ./data/InstaPy
 
+cp dotEnv project/.env
+
 cd project
+npm i
 npm run front:build
